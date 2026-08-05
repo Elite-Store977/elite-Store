82bdfcc2da715ebcdb46ae7909c698617d76d72d
Elite-Store977 <faiqfaeez@gmail.com>
2026-08-05 20:47:22 +0500

    Create product.js

---
+++ product.js
@@ -0,0 +1,35 @@
+const products = [
+
+{
+    name:"Smart Watch",
+    price:50,
+    image:"images/watch.webp",
+    category:"watch",
+    description:"Premium Smart Watch with fitness tracking."
+},
+
+{
+    name:"Wireless Earbuds",
+    price:35,
+    image:"images/earbuds.jpg",
+    category:"earbuds",
+    description:"Crystal clear sound with long battery."
+},
+
+{
+    name:"Gaming Mouse",
+    price:25,
+    image:"images/mouse.webp",
+    category:"gaming",
+    description:"RGB Gaming Mouse with high precision."
+},
+
+{
+    name:"Bluetooth Speaker",
+    price:45,
+    image:"images/speaker.webp",
+    category:"speaker",
+    description:"Portable speaker with deep bass."
+}
+
+];
